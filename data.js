f0f22041267952263a19d1254c7aafbcf6c5ee23
stannardh <hilaryvstannard@gmail.com>
2020-12-14 21:02:49 +0100

    added date and time function

--- data.js
+++ data.js
@@ -1,5 +1,23 @@
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (minutes < 10) {
+minutes = `0${minutes}`
+};
+if (hours < 10) {
+hours = `0${hours}`
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day}, ${hours}:${minutes}`
+
+}
+
+
+
+
 function displayTemperature(response){
-    console.log(response)
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = response.data.name;
 let temperatureElement = document.querySelector("#temperature");
@@ -9,7 +27,9 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 let humidityElement = document.querySelector("#humidity");
 humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
 let windElement = document.querySelector("#wind");
-windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`
+windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+let dateElement = document.querySelector("#time");
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
