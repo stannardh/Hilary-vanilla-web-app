e488989b39c08cfba58937c4fd14c2bc6d67dad5
stannardh <hilaryvstannard@gmail.com>
2020-12-15 18:34:17 +0100

    Added icon elements

--- data.js
+++ data.js
@@ -30,11 +30,15 @@ let windElement = document.querySelector("#wind");
 windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
 let dateElement = document.querySelector("#time");
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description)
+
 }
 
 
 let apiKey = "4c3c671c267c67d5291f03e40c1f4165";
-let city = "London"
+let city = "Hong Kong"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
